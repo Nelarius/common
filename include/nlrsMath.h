4b5b0b023c2c8acc3998ff2c01b32d521f933954
Johann Muszynski <johann.muszynski@gmail.com>
2017-03-06 23:43:10 +0200

    Add angle conversion functions

--- include/nlrsMath.h
+++ include/nlrsMath.h
@@ -6,6 +6,18 @@ namespace nlrs
 {
 
 template<typename T>
+constexpr T degreesToRadians(T degrees)
+{
+    return (degrees * T(3.14159265358979323846)) / T(180);
+}
+
+template<typename T>
+constexpr T radiansToDegrees(T radians)
+{
+    return (radians * T(180)) / T(3.14159265358979323846);
+}
+
+template<typename T>
 T clamp(T t, T min, T max)
 {
     return std::max(min, std::min(t, max));
